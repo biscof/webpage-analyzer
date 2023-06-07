66827f3eaf0ba09ed6814477a8b3b89aa9096c59
maksim butakov <qulifis@gmail.com>
2023-06-07 12:50:34 +0600

    Add dockerfile

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,36 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8000");
+        return Integer.valueOf(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> {
+            ctx.result("Hello World");
+        });
+//        app.routes(() -> {
+//
+//        });
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        addRoutes(app);
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
